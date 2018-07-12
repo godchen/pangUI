bd28a3596ff0c7ca52ddacabac1845a8920c7132
godchen <7721929933@qq.com>
2018-07-12 09:29:30 +0800

    component aside and aside.scss

---
+++ packages/aside/src/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="pang-aside" :style="{ width }">
+    <slot></slot>
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: 'PANGAside',
+    componentName: 'PANGAside',
+    props: {
+      width: {
+        type: String,
+        default: '300px'
+      }
+    }
+  };
+</script>
